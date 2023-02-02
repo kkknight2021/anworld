d2991429d9a7d7d245ef5f4d0cf2fb99d418a1be
LDY <ldy1456714026@outlook.com>
2023-02-02 19:18:45 +0800

    将天地图接口转为懒加载
    - 去除index.html中引入天地图接口的标签
    - 在MapTest.vue中添加动态引入接口的功能, 通过tdtLoaded属性通知TdtMap
    - TdtMap.vue中通过判断tdtLoaded Prop进行初始化

--- src/components/MapTest.vue
+++ src/components/MapTest.vue
@@ -1,10 +1,13 @@
 <template>
-  <TdtMap></TdtMap>
+  <TdtMap v-bind:tdtLoaded="tdtLoaded"></TdtMap>
 </template>
 
 <script>
   /* 导入组件 */
   import TdtMap from '../common/TdtMap.vue'
+  // const TdtMap = ()=>import('../common/TdtMap.vue')
+
+  
   
   export default {
     name: 'MapTest',
@@ -14,12 +17,42 @@
     },
     data() {
       return {
+        tdtLoaded: false,
       }
     },
+    beforeCreate(){
+      
+    },
     created() {
+      // 懒加载天地图接口
+      const tdtScriptId = 'dynamic-tdt-imp';
+      // console.log('beforeCreateMapTest');
+
+      if (!document.getElementById(tdtScriptId)){
+        // console.log('init tdt script!')
+        // 操作Dom插入标签
+        // this.initTdtScript(tdtScriptId);
+
+        let script = document.createElement('script');
+        script.type = "text/javascript";
+        script.src = 'http://api.tianditu.gov.cn/api?v=4.0&tk=5017a8687f519b8ae81717aa42f706ff';
+        script.id = tdtScriptId;
+        document.body.appendChild(script);
+        script.onload = ()=>{this.tdtLoaded = true;}
+        script.onerror = ()=>{console.log("获取失败!")}
+
+      }else{
+        // 存在标签, 则默认已加载接口
+        this.tdtLoaded = true;
+      }
+      // console.log('created: '+this.$bus.tdtLoaded);
+
     },
     mounted(){
     },
+    beforeUnmount(){
+    },
+    
     watch: {
     },
     methods: {
